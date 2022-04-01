8da3c90dc0c6da3fde7179b5fcaaad8e154655bd
Anais <anaisassamoi02@gmail.com>
2022-04-01 17:18:35 +0200

    complete readial chart

--- src/components/rader-chart/RadarChart.js
+++ src/components/rader-chart/RadarChart.js
@@ -1,150 +1,135 @@
 import * as d3 from "d3";
 import React from "react";
 import PropTypes from "prop-types";
+import "./RadarChart.css"
 
 export default function RadarChart({perf}) {
   var axes = perf.map(p => {
     return {
-      "axis": p.kind,
+      "axis": p.kind[0].toUpperCase() + p.kind.substring(1),
       "value": p.value,
     }
   })
   var newData = [axes];
+  var width = 200;
+  var height = 200;
   console.log(newData)
 
-  var config = {
-    w: 300,
-    h: 300,
-    maxValue: 250,
-    levels: 6,
-    ExtraWidthX: 300
-  }
-
-  var svg = d3.select('body')
-    .selectAll('svg')
-    .append('svg')
-    .attr("width", 300)
-    .attr("height", 300);
-
   var RadarChart = {
-    draw: function(id, d, options){
+    /**
+     * @description draw radial chart
+     * @param {string} select class or id of select div
+     * @param {object[]} data user performance data
+     * @param {object} config chart configurartion parameters
+    */
+    draw: function(select, data, config) {
       var cfg = {
-       radius: 5,
-       w: 600,
-       h: 600,
-       factor: 1,
-       factorLegend: .85,
-       levels: 3,
-       maxValue: 0,
-       radians: 2 * Math.PI,
-       opacityArea: 0.5,
-       ToRight: 5,
-       TranslateX: 80,
-       TranslateY: 30,
-       ExtraWidthX: 100,
-       ExtraWidthY: 100,
-       color: d3.scaleOrdinal().range(["red", "red"])
+        radius: 5,
+        w: 250,
+        h: 300,
+        factor: 1,
+        factorLegend: .85,
+        levels: 5,
+        maxValue: 240,
+        radians: 2 * Math.PI,
+        TranslateX: 60,
+        TranslateY: 50,
+        ExtraWidthX: 120,
+        ExtraWidthY: 100,
       };
     
-    if('undefined' !== typeof options){
-      for(var i in options){
-      if('undefined' !== typeof options[i]){
-        cfg[i] = options[i];
+      if('undefined' !== typeof config){
+        for(var i in config){
+          if('undefined' !== typeof config[i]){
+            cfg[i] = config[i];
+          }
+        }
       }
-      }
-    }
-      
-    cfg.maxValue = 250;
-      
-      var allAxis = (d[0].map(function(i, j){return i.axis}));
+            
+      var allAxis = (data[0].map(i => {return i.axis }));
       var total = allAxis.length;
       var radius = cfg.factor*Math.min(cfg.w/2, cfg.h/2);
-      d3.select(id).select("svg").remove();
+      d3.select(select).select("svg").remove();
   
-      var g = d3.select(id)
-          .append("svg")
-          .attr("width", cfg.w+cfg.ExtraWidthX)
-          .attr("height", cfg.h+cfg.ExtraWidthY)
-          .append("g")
-          .attr("transform", "translate(" + cfg.TranslateX + "," + cfg.TranslateY + ")");
+      // set svg and group
+      var g = d3.select(select)
+      .append("svg")
+      .attr("width", cfg.w+cfg.ExtraWidthX)
+      .attr("height", cfg.h+cfg.ExtraWidthY)
+      .attr("class", "bg")
+      .append("g")
+      .attr("transform", "translate(" + cfg.TranslateX + "," + cfg.TranslateY + ")");
 
-  
-      let series = 0;
-  
-      var axis = g.selectAll(".axis")
-          .data(allAxis)
-          .enter()
-          .append("g")
-          .attr("class", "axis");
-  
-      axis.append("line")
-        .attr("x1", cfg.w/2)
-        .attr("y1", cfg.h/2)
-        .attr("x2", function(d, i){return cfg.w/2*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
-        .attr("y2", function(d, i){return cfg.h/2*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
+      // add circular segments
+      for(var s=0; s<cfg.levels; s++){
+        var levelFactor = cfg.factor*radius*((s+1)/cfg.levels);
+        g.selectAll(".levels")
+        .data(allAxis)
+        .enter()
+        .append("svg:line")
+        .attr("x1", function(data, i){return levelFactor*(1-cfg.factor*Math.sin(i*cfg.radians/total));})
+        .attr("y1", function(data, i){return levelFactor*(1-cfg.factor*Math.cos(i*cfg.radians/total));})
+        .attr("x2", function(data, i){return levelFactor*(1-cfg.factor*Math.sin((i+1)*cfg.radians/total));})
+        .attr("y2", function(data, i){return levelFactor*(1-cfg.factor*Math.cos((i+1)*cfg.radians/total));})
         .attr("class", "line")
-        .style("stroke", "grey")
-        .style("stroke-width", "1px");
+        .style("stroke", "white")
+        .style("stroke-width", "1px")
+        .attr("transform", "translate(" + (cfg.w/2-levelFactor) + ", " + (cfg.h/2-levelFactor) + ")");
+      }
   
+      // add axis
+      var axis = g.selectAll(".axis")
+      .data(allAxis)
+      .enter()
+      .append("g")
+      .attr("class", "axis");
+    
+      // add axis title
       axis.append("text")
-        .attr("class", "legend")
-        .text(function(d){return d})
-        .style("font-family", "sans-serif")
-        .style("font-size", "11px")
-        .attr("text-anchor", "middle")
-        .attr("dy", "1.5em")
-        .attr("transform", function(d, i){return "translate(0, -10)"})
-        .attr("x", function(d, i){return cfg.w/2*(1-cfg.factorLegend*Math.sin(i*cfg.radians/total))-60*Math.sin(i*cfg.radians/total);})
-        .attr("y", function(d, i){return cfg.h/2*(1-Math.cos(i*cfg.radians/total))-20*Math.cos(i*cfg.radians/total);});
-  
-   
-      d.forEach(function(y, x){
+      .attr("class", "legend")
+      .text(function(data){return data})
+      .style("font-size", "14px")
+      .style("fill", "white")
+      .attr("text-anchor", "middle")
+      .attr("dy", "20px")
+      .attr("transform", function(data, i){return "translate(0, -20)"})
+      .attr("x", function(data, i){return cfg.w/2*(1-cfg.factorLegend*Math.sin(i*cfg.radians/total))-60*Math.sin(i*cfg.radians/total);})
+      .attr("y", function(data, i){return cfg.h/2*(1-Math.cos(i*cfg.radians/total))-20*Math.cos(i*cfg.radians/total);});
+    
+      // get performance dataset
+      data.forEach(function(y, x){
         let dataValues = [];
         g.selectAll(".nodes")
         .data(y, function(j, i){
           dataValues.push([
           cfg.w/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.sin(i*cfg.radians/total)), 
           cfg.h/2*(1-(parseFloat(Math.max(j.value, 0))/cfg.maxValue)*cfg.factor*Math.cos(i*cfg.radians/total))
-          ]);
-        });
-        dataValues.push(dataValues[0]);
-        g.selectAll(".area")
-               .data([dataValues])
-               .enter()
-               .append("polygon")
-               .attr("class", "radar-chart-serie"+series)
-               .style("stroke-width", "2px")
-               .style("stroke", cfg.color(series))
-               .attr("points",function(d) {
-                 var str="";
-                 for(var pti=0;pti<d.length;pti++){
-                   str=str+d[pti][0]+","+d[pti][1]+" ";
-                 }
-                 return str;
-                })
-               .style("fill", function(j, i){return cfg.color(series)})
-               .style("fill-opacity", cfg.opacityArea)
-               .on('mouseover', function (d){
-                        var z = "polygon."+d3.select(this).attr("class");
-                        g.selectAll("polygon")
-                         .transition(200)
-                         .style("fill-opacity", 0.1); 
-                        g.selectAll(z)
-                         .transition(200)
-                         .style("fill-opacity", .7);
-                        })
-               .on('mouseout', function(){
-                        g.selectAll("polygon")
-                         .transition(200)
-                         .style("fill-opacity", cfg.opacityArea);
-               });
-        series++;
+        ]);
       });
-      series=0;
-  
 
-      }
+      // add performance area
+      g.selectAll(".area")
+      .data([dataValues])
+      .enter()
+      .append("polygon")
+      .attr("points",function(d) {
+        var str="";
+        for(var pti=0;pti<d.length;pti++) {
+          str=str+d[pti][0]+","+d[pti][1]+" ";
+        }
+        return str;
+      })
+      .style("fill", "#FF0101B2")
+      });
+    }
   };
+
+  var config = {
+    w: width,
+    h: height,
+    maxValue: 240,
+    levels: 5,
+  }
 
   RadarChart.draw(".radar-chart", newData, config);
 
@@ -153,4 +138,8 @@ export default function RadarChart({perf}) {
       <div className="radar-chart"></div>
     </section>
   );
+}
+
+RadarChart.prototype = {
+  sessions: PropTypes.arrayOf(PropTypes.object),
 }
